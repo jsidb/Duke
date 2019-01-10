f286a5f506da72e1a0e5b89925a7f854b2225fab
Jens Schulte in den Bäumen <j.schulte@oneway.de>
2019-01-10 17:21:15 +0100

    TASK: Change Lucene dep to 6.4.2

--- duke-lucene/src/test/java/no/priv/garshol/duke/databases/ExtraLuceneDatabaseTest.java
+++ duke-lucene/src/test/java/no/priv/garshol/duke/databases/ExtraLuceneDatabaseTest.java
@@ -1,10 +1,11 @@
 
 package no.priv.garshol.duke.databases;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -12,59 +13,51 @@ import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.util.Version;
+import org.junit.Test;
 
-import no.priv.garshol.duke.Configuration;
 import no.priv.garshol.duke.ConfigurationImpl;
+import no.priv.garshol.duke.DukeException;
 import no.priv.garshol.duke.Property;
 import no.priv.garshol.duke.PropertyImpl;
 import no.priv.garshol.duke.Record;
 import no.priv.garshol.duke.RecordImpl;
-import no.priv.garshol.duke.DukeException;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public class ExtraLuceneDatabaseTest {
 
-  @Test
-  public void testLockedIndex() throws IOException {
-    // this test verifies that we don't wind up in an inconsistent state
-    // when the index we want to work with is already locked
+	@Test
+	public void testLockedIndex() throws IOException {
+		// this test verifies that we don't wind up in an inconsistent state
+		// when the index we want to work with is already locked
 
-    // make a locked index
-    Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_CURRENT);
-    IndexWriterConfig cfg =
-      new IndexWriterConfig(Version.LUCENE_CURRENT, analyzer);
-    File tmp = new File(System.getProperty("java.io.tmpdir"),
-                        "lucene-temp-" + (Math.random() * 100000));
-    Directory directory = FSDirectory.open(tmp);
-    IndexWriter writer = new IndexWriter(directory, cfg);
+		// make a locked index
+		Analyzer analyzer = new StandardAnalyzer();
+		IndexWriterConfig cfg = new IndexWriterConfig(analyzer);
+		Path tmp = Paths.get(System.getProperty("java.io.tmpdir"), "lucene-temp-" + (Math.random() * 100000));
 
-    // now try to open a LuceneDatabase in the same place
-    List<Property> properties = new ArrayList();
-    properties.add(new PropertyImpl("id"));
-    ConfigurationImpl config = new ConfigurationImpl();
-    config.setProperties(properties);
-    LuceneDatabase db = new LuceneDatabase();
-    db.setPath(tmp.getAbsolutePath());
-    db.setConfiguration(config);
+		Directory directory = FSDirectory.open(tmp);
+		IndexWriter writer = new IndexWriter(directory, cfg);
 
-    Record r = new RecordImpl();
-    try {
-      db.index(r);
-    } catch (DukeException e) {
-      // this is expected
-    }
+		// now try to open a LuceneDatabase in the same place
+		List<Property> properties = new ArrayList();
+		properties.add(new PropertyImpl("id"));
+		ConfigurationImpl config = new ConfigurationImpl();
+		config.setProperties(properties);
+		LuceneDatabase db = new LuceneDatabase();
+		db.setPath(tmp.toAbsolutePath().toString());
+		db.setConfiguration(config);
 
-    try {
-      db.index(r);
-    } catch (DukeException e) {
-      // this is also expected
-    }
-  }
+		Record r = new RecordImpl();
+		try {
+			db.index(r);
+		} catch (DukeException e) {
+			// this is expected
+		}
+
+		try {
+			db.index(r);
+		} catch (DukeException e) {
+			// this is also expected
+		}
+	}
 
 }
